9547bfaa9adabd344dab9fb4e45ec9d725c9305b
ahmed025566 <ahmedsalah025566@gmail.com>
2023-10-25 19:13:58 +0300

    Process the data

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
+  after_create :update_likes_counter
   belongs_to :user
   belongs_to :post
+
+  def update_likes_counter
+    post.update(LikesCounter: +1)
+  end
 end

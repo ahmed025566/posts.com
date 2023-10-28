6bb79c91066178e9272124c65f2c74d03f33fd2c
ahmed025566 <ahmedsalah025566@gmail.com>
2023-10-28 14:53:36 +0200

    Add like model specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,7 +6,7 @@ describe 'Comment Model Tests' do
 
   it 'should increment the comments counter by 1 for a post' do
     expect(post.CommentsCounter).to_not be_nil
-    comment = Comment.create(user: user, post:, Text: 'Hi Tom!')
+    comment = Comment.create(user:, post:, Text: 'Hi Tom!')
     expect(post.CommentsCounter).to eql(1)
     expect(comment.user).to eql(user)
     expect(post.comments).to include(comment)

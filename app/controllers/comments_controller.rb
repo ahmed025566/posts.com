5b78c4064fb07373b4f23a1fc8ad1fb1a7d65005
ahmed025566 <ahmedsalah025566@gmail.com>
2023-11-09 19:53:51 +0200

    Add comment destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to user_post_path(user: current_user, post: params[:post_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:Text)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ApplicationRecord
   validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nill: true }
   validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nill: true }
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def update_posts_counter
     author.update!(PostsCounter: author.posts.count)
